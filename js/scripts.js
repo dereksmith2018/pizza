dad202d545c04f17957076f37116ecfa567e7e5c
Derek <kingdereksmith@gmail.com>
2018-08-31 14:36:08 -0700

    changed html layout and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
 // backend
 var price = 0;
 
-function Pizza(type,toppings,price) {
+function Pizza(type,toppings) {
   this.type = type;
   this.toppings = toppings;
   this.price = 0;
 }
 
-var typeValue = {"thin":10,"regular":12,"deepdish":14}
-var toppingsValue = {"cheese":1,"pepperoni":1,"cBacon":1,"sausage":1,"bacon":1,"ham":1,"Mushrooms":1,"jalapeno":1, "peppers":1, "olive":1, "pineapple":1}
+// var typeValue = {"thin":10,"regular":12,"deepdish":14}
+// var toppingsValue = {"cheese":1,"pepperoni":1,"cBacon":1,"sausage":1,"bacon":1,"ham":1,"Mushrooms":1,"jalapeno":1, "peppers":1, "olive":1, "pineapple":1}
 
 Pizza.prototype.getPrice = function() {
-  var totalForToppings = 0;
-  this.toppings.forEach(function() {
-    totalForToppings += toppingsPrice[];
-  });
-
-  this.price = (sizePrice[this.type] + totalForToppings);
-  return this.price;
+   if (this.type === "thin") {
+     this.price = this.toppings.length + 10;
+     return this.price
+   }
+   else if(this.type === "regular") {
+     this.price = this.toppings.length + 12
+     return this.price
+   } else {
+     this.price = this.toppings.length + 14
+     return this.price
+   }
+   console.log(getPrice)
 }
 
 //user logic
@@ -26,7 +31,7 @@ $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
 
-    var size = $("input[type=radio][name=type]:checked").val();
+    var type = $("input[type=radio][name=type]:checked").val();
     var toppings = [];
     $("input[type=checkbox][name=toppings]:checked").each(function() {
       toppings.push($(this).val());
