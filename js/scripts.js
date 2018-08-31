cce05cd406e9d99ebe65c5c2555c4e4266d31494
Derek <kingdereksmith@gmail.com>
2018-08-31 16:03:41 -0700

    fixed js to display entery with selected items

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function Pizza(type,toppings) {
 // var typeValue = {"thin":10,"regular":12,"deepdish":14}
 // var toppingsValue = {"cheese":1,"pepperoni":1,"cBacon":1,"sausage":1,"bacon":1,"ham":1,"Mushrooms":1,"jalapeno":1, "peppers":1, "olive":1, "pineapple":1}
 
-Pizza.prototype.getPrice = function() {
+Pizza.prototype.getOrder = function() {
    if (this.type === "thin") {
      this.price = this.toppings.length + 10;
      return this.price
@@ -22,7 +22,6 @@ Pizza.prototype.getPrice = function() {
      this.price = this.toppings.length + 14
      return this.price
    }
-   console.log(getPrice)
 }
 
 //user logic
@@ -32,18 +31,21 @@ $(document).ready(function() {
     event.preventDefault();
 
     var type = $("input[type=radio][name=type]:checked").val();
+    console.log(type)
     var toppings = [];
     $("input[type=checkbox][name=toppings]:checked").each(function() {
       toppings.push($(this).val());
+      console.log($(this).val());
     });
 
     var newPizza = new Pizza(type,toppings,price);
-    newPizza.getPrice();
+    newPizza.getOrder();
 
-
+    document.getElementById("answer").innerHTML = newPizza.getOrder();
+    alert("You ordered a " + newPizza.type + " with " + newPizza.toppings + " and your total is $" + newPizza.price);
 
   });
-  $("#answer").click(function(){
-    alert("working");
-  });
+  // $("#answer").click(function(){
+  //   alert("working");
+  // });
 });
